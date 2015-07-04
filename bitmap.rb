7bedc55feeaf96ba3547562970219f212bf16a46
Simon Yu <simonykq@gmail.com>
2015-07-05 00:44:46 +0100

    add method in bitmaps for reseting and setting color for a specific coordinate

--- bitmap.rb
+++ bitmap.rb
@@ -1,15 +1,30 @@
 class Bitmap
 
+	attr_accessor :rows, :columns
 	attr_accessor :coordinate
 
     def initialize x, y, color
     	raise "Invalid co-ordinate inputs" unless is_valid?(x, y)
-    	@coordinate = Array.new(x){ Array.new(y) { 'O' } }
+    	@rows, @columns = [x, y]
+    	@coordinate = Array.new(@columns){ Array.new(y) { 'O' } }
     end	
 
     def self.new x=250, y=250,color='O'
     	super x, y, color
-    end		
+    end	
+
+    def C color='O'
+    	@coordinate.each do |row|
+    		row.each do |cell|
+ 				cell = color
+    		end	
+    	end	
+    end	
+
+    def L x, y, color
+    	raise "Index out of bound" unless x.between?(0, @rows-1) && y.between?(0, @columns-1)
+    	@coordinate[x][y] = color
+    end	
 
 	private
 
