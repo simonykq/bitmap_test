256b478075dac6800f372659bc13cc4803fb228f
Simon Yu <simonykq@gmail.com>
2015-07-05 12:40:11 +0100

    Add default values for L, V and H methods

--- bitmap.rb
+++ bitmap.rb
@@ -21,12 +21,12 @@ class Bitmap
     	end	
     end	
 
-    def L x, y, color
+    def L x=0, y=0, color='O'
     	raise "Index out of bound" unless x.between?(1, @cols) && y.between?(1, @rows)
     	@coordinates[x-1][y-1] = color
     end	
 
-    def V x, y_1, y_2, color
+    def V x=0, y_1=0, y_2=0, color='O'
     	raise "Index out of bound" unless x.between?(1, @cols) && [y_1, y_2].all?{|num| num.between? 1, @rows }
     	range = y_2 >= y_1 ? y_1..y_2 : y_2..y_1
 		for i in range do
@@ -34,7 +34,7 @@ class Bitmap
 		end		
     end	
 
-    def H x_1, x_2, y, color
+    def H x_1=0, x_2=0, y=0, color='O'
     	raise "Index out of bound" unless [x_1, x_2].all?{|num| num.between? 1, @cols } && y.between?(1, @rows)
     	range = x_2 > x_1 ? x_1..x_2 : x2..x_1
     	for i in range do
