720603f7c9b0df30785f000abd5489851f0e8411
Simon Yu <simonykq@gmail.com>
2015-07-05 01:12:29 +0100

    renaming x, y in constructor to m, n. pluralize coordinate

--- bitmap.rb
+++ bitmap.rb
@@ -1,20 +1,20 @@
 class Bitmap
 
-	attr_accessor :rows, :columns
-	attr_accessor :coordinate
+	attr_accessor :rows, :cols
+	attr_accessor :coordinates
 
-    def initialize x, y, color
-    	raise "Invalid co-ordinate inputs" unless is_valid?(x, y)
-    	@rows, @columns = [x, y]
-    	@coordinate = Array.new(@columns){ Array.new(y) { 'O' } }
+    def initialize m, n, color
+    	raise "Invalid co-ordinate inputs" unless is_valid?(m, n)
+    	@cols, @rows = [m, n]
+    	@coordinates = Array.new(@rows){ Array.new(@cols) { 'O' } }
     end	
 
-    def self.new x=250, y=250,color='O'
-    	super x, y, color
+    def self.new m=250, n=250,color='O'
+    	super m, n, color
     end	
 
     def C color='O'
-    	@coordinate.each do |row|
+    	@coordinates.each do |row|
     		row.each do |cell|
  				cell = color
     		end	
@@ -23,14 +23,17 @@ class Bitmap
 
     def L x, y, color
     	raise "Index out of bound" unless x.between?(0, @rows-1) && y.between?(0, @columns-1)
-    	@coordinate[x][y] = color
-    end	
+    	@coordinates[x][y] = color
+    end		
 
 	private
 
-	def is_valid? x, y
-		x_y = [x, y]
-		x_y.all? {|num| num.is_a? Integer } && x_y.all? {|num| num.between? 1, 250 }
+	def is_valid? m, m
+		m_n = [m, n]
+		m_n.all? {|num| num.is_a? Integer } && m_n.all? {|num| num.between? 1, 250 }
+	end	
+
+
 	end	
 
 end	
