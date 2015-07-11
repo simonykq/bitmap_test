d35c9084312a6b3e3bf45d482af2028fe21c9d5a
Simon Yu <simonykq@gmail.com>
2015-07-12 00:02:21 +0100

    Don't assign default values for L V H and F methods

--- bitmap.rb
+++ bitmap.rb
@@ -21,13 +21,13 @@ class Bitmap
         end 
     end 
 
-    def L x=0, y=0, color='O'
+    def L x, y, color='O'
         x, y = [x.to_i, y.to_i]
         return unless x.between?(1, @cols) && y.between?(1, @rows)
         @coordinates[x-1][y-1] = color
     end 
 
-    def V x=0, y_1=0, y_2=0, color='O'
+    def V x, y_1, y_2, color='O'
         x, y_1, y_2 = [x.to_i, y_1.to_i, y_2.to_i]
         return unless x.between?(1, @cols) && [y_1, y_2].all?{|num| num.between? 1, @rows }
         range = y_2 >= y_1 ? y_1..y_2 : y_2..y_1
@@ -36,7 +36,7 @@ class Bitmap
         end     
     end 
 
-    def H x_1=0, x_2=0, y=0, color='O'
+    def H x_1, x_2, y, color='O'
         x_1, x_2, y = [x_1.to_i, x_2.to_i, y.to_i]
         return unless [x_1, x_2].all?{|num| num.between? 1, @cols } && y.between?(1, @rows)
         range = x_2 > x_1 ? x_1..x_2 : x2..x_1
@@ -45,7 +45,7 @@ class Bitmap
         end 
     end 
 
-    def F x=0, y=0, color='O'
+    def F x, y, color='O'
         x, y = [x.to_i, y.to_i]
         target_color = @coordinates[x-1][y-1]
         flood_fill x, y, target_color, color
