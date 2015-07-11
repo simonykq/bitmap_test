9d0f5885913edb41da8e775f6c4d92cc7b506167
Simon Yu <simonykq@gmail.com>
2015-07-11 20:01:37 +0100

    Wrap up all the command calls in the main loop etc

--- main.rb
+++ main.rb
@@ -1,4 +1,4 @@
-require 'bitmap'
+require './bitmap'
 
 loop do
 	printf '> '
@@ -6,21 +6,27 @@ loop do
 	command, *params = inputs.split /\s/
 	case command
 	when 'I'
-	  puts params	
-	when 'C'
-	  puts params	
-	when 'L'
-	  puts params			
-	when 'V'
-	  puts params			
-	when 'H'
-	  puts params	
-	when 'F'
-	  puts params			
-	when 'S'	
-	  puts params		
+		begin
+			@bitmap = Bitmap.new *params
+		rescue => e
+			puts e.message
+			puts e.backtrace.join("\n")
+		end	
+	when 'C', 'L', 'V', 'H', 'F', 'S'
+		if !@bitmap
+			puts "Bitmap not initialized. Please use I command to initialize bitmap first"
+		else
+			begin
+				@bitmap.send command, *params
+			rescue => e
+				puts e.message
+				puts e.backtrace.join("\n")
+			end		
+		end		
 	when 'X', 'exit'
 		puts "Bye bye!"
 		break
+	else
+		puts "Unrecognized command"		
 	end	
 end	
