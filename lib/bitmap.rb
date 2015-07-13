618303f1295c02aa75b5188219be98b4c4100866
Simon Yu <simonykq@gmail.com>
2015-07-13 18:42:12 +0100

    Refactor some code to get and set a specific pixels

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -14,44 +14,40 @@ class Bitmap
     end 
 
     def C color='O'
-        @coordinates.each do |column|
-            column.each do |cell|
-                cell = color
-            end 
-        end 
+        for y in 1..@rows do
+            for x in 1..@cols do
+                set_pixel x, y, color
+            end    
+        end    
     end 
 
     def L x, y, color='O'
-        raise "Invalid x or y inputs" unless is_valid_integer_between?(1, @cols, x) && is_valid_integer_between?(1, @rows, y)
-        @coordinates[x-1][y-1] = color
+        set_pixel x, y, color
     end 
 
     def V x, y_1, y_2, color='O'
-        raise "Invalid x or y inputs" unless is_valid_integer_between?(1, @cols, x) && is_valid_integer_between?(1, @rows, y_1, y_2)
         range = y_2 >= y_1 ? y_1..y_2 : y_2..y_1
         for i in range do
-            @coordinates[x-1][i-1] = color
+            set_pixel x, i, color
         end     
     end 
 
     def H x_1, x_2, y, color='O'
-        raise "Invalid x or y inputs" unless is_valid_integer_between?(1, @cols, x_1, x_2) && is_valid_integer_between?(1, @rows, y)
         range = x_2 > x_1 ? x_1..x_2 : x2..x_1
         for i in range do
-            @coordinates[i-1][y-1] = color
+            set_pixel i, y, color
         end 
     end 
 
     def F x, y, color='O'
-        raise "Invalid x or y inputs" unless is_valid_integer_between?(1, @cols, x) && is_valid_integer_between?(1, @rows, y)
-        target_color = @coordinates[x-1][y-1]
+        target_color = get_pixel x, y
         flood_fill x, y, target_color, color
     end 
 
     def S
         for y in 1..@rows do
             for x in 1..@cols do
-                printf @coordinates[x-1][y-1]
+                printf get_pixel x, y
             end  
             printf "\n"  
         end 
@@ -59,19 +55,33 @@ class Bitmap
 
     private
 
+    def set_pixel x, y, color
+        raise "Invalid x or y inputs" unless is_valid_integer_between?(1, @cols, x) && is_valid_integer_between?(1, @rows, y)
+        @coordinates[x-1][y-1] = color
+    end 
+
+    def get_pixel x, y
+        raise "Invalid x or y inputs" unless is_valid_integer_between?(1, @cols, x) && is_valid_integer_between?(1, @rows, y)
+        @coordinates[x-1][y-1]
+    end 
+
     def is_valid_integer_between? lower_bound, upper_bound, *numbers
         numbers.all? {|num| num.is_a? Integer } && numbers.all? {|num| num.between? lower_bound, upper_bound }
     end    
 
     def flood_fill x, y, target_color, replace_color
-        return unless x.between?(1, @cols) && y.between?(1, @rows)
-        return if @coordinates[x-1][y-1] != target_color
-        return if @coordinates[x-1][y-1] == replace_color
-        @coordinates[x-1][y-1] = replace_color
-        flood_fill x-1, y, target_color, replace_color
-        flood_fill x+1, y, target_color, replace_color
-        flood_fill x, y-1, target_color, replace_color
-        flood_fill x, y+1, target_color, replace_color
+        begin
+            color = get_pixel x, y
+            return if color != target_color
+            return if color == replace_color
+            set_pixel x, y, replace_color
+            flood_fill x-1, y, target_color, replace_color
+            flood_fill x+1, y, target_color, replace_color
+            flood_fill x, y-1, target_color, replace_color
+            flood_fill x, y+1, target_color, replace_color
+        rescue
+            return
+        end    
     end 
 
 end 
